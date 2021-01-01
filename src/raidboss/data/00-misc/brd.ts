9bcb92912bd5c5d004440f636e75f86bc4dc21cb
Maiko Tan <maiko.tan.coding@gmail.com>
2021-01-01 22:00:11 +0800

    feat: add brd dots

--- src/raidboss/data/00-misc/brd.ts
+++ src/raidboss/data/00-misc/brd.ts
@@ -1,12 +1,14 @@
 import Conditions from "Conditions";
 import NetRegexes from "NetRegexes";
 import ZoneId from "ZoneId";
+import { createDotDurationTrigger } from "src/util/index";
 
 const triggerSet: TriggerFile = {
   zoneId: ZoneId.MatchAll,
   triggers: [
     {
       id: 'Personal Bard Barrage',
+      // 6B: Barrage
       netRegex: NetRegexes.ability({ id: '6B' }),
       condition: Conditions.targetIsYou(),
       preRun: (data) => data.buffStraightShot = true,
@@ -24,6 +26,29 @@ const triggerSet: TriggerFile = {
       condition: (data, matches) => matches.source === data.me,
       preRun: (data) => data.buffStraightShot = false,
     },
+    {
+      id: 'Personal Bard Songs',
+      // 71:   Mage's Ballad
+      // 74:   Army's Paeon
+      // DE7:  The Wanderer's Minuet
+      netRegex: NetRegexes.ability({ id: ['71', '74', 'DE7'] }),
+      condition: (data, matches) => matches.source === data.me,
+      preRun: function (data, matches) {
+        const nextSongs: any = {
+          '71': 'Army\'s Paeon',
+          '74': 'The Wanderer\'s Minuet',
+          'DE7': 'Mage\'s Ballad',
+        };
+        const nextSong = nextSongs[matches.id];
+      },
+    },
+    // DoTs
+    ...([
+      { id: '7C', name: 'Venomous Bite', info: 'コスティク' },
+      { id: '81', name: 'Windbite', info: 'ストーム' },
+      { id: '4B0', name: 'Caustic Bite', info: 'コスティク' },
+      { id: '4B1', name: 'Stormbite', info: 'ストーム' },
+    ].map((elem) => createDotDurationTrigger('Bard', elem.id, elem.name, elem.info))),
   ],
 };
 
---
+++ src/util/index.ts
@@ -0,0 +1,13 @@
+import NetRegexes from "NetRegexes";
+
+export const createDotDurationTrigger = (prefix: string, id: string, name: string, info: string | Locale): Trigger => {
+  return {
+    id: `Personal ${prefix} DoT ${name}`,
+    netRegex: NetRegexes.gainsEffect({ effectId: id }),
+    condition: (data, matches) => matches.source === data.me,
+    preRun: function (data, matches) {
+      data.lastDotTime = new Date();
+      data.lastDotDuration = matches.duration;
+    },
+  };
+};
