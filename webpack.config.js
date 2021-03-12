d8d68d922d4fe3d2a98596edda4e5f6cbe6adfb2
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2021-03-12 13:12:46 +0000

    fix: add more external variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    jobs: './src/jobs/index.ts',
-    oopsyraidsy: './src/oopsyraidsy/index.ts',
-    radar: './src/radar/index.ts',
-    raidboss: './src/raidboss/index.ts'
+    jobs: "./src/jobs/index.ts",
+    oopsyraidsy: "./src/oopsyraidsy/index.ts",
+    radar: "./src/radar/index.ts",
+    raidboss: "./src/raidboss/index.ts"
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   externals: {
     Conditions: "Conditions",
+    ContentType: "ContentType",
     NetRegexes: "NetRegexes",
     Options: "Options",
+    Outputs: "Outputs",
     Regexes: "Regexes",
     Responses: "Responses",
+    Util: "Util",
     ZoneId: "ZoneId",
+    ZoneInfo: "ZoneInfo",
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
