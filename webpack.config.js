b7958b06fb2735ca62d37dcd893468ea50d636a8
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2021-09-02 10:09:51 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    jobs: "./src/jobs/index.ts",
+    oopsyraidsy: "./src/oopsyraidsy/index.ts",
+    radar: "./src/radar/index.ts",
+    raidboss: "./src/raidboss/index.ts",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|cjs|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    chrome: "75",
+                  },
+                },
+              ],
+              ["@babel/preset-typescript"],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.txt$/,
+        exclude: /node_modules/,
+        type: 'asset/source',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.CleanPlugin(),
+  ],
+};
